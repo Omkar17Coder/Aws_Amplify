fe086fbc1fbc6aa7a0e7de028462787068f9b7f3
Your Name <you@example.com>
2023-09-30 19:37:44 +0530

    added new lines

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello  From  Aws Amplify THIS IS HOSTED TO AWS AMPLIFY
         </p>
         <a
           className="App-link"
